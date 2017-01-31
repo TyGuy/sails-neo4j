5e09d8d475b8b60efda8e9da2875bee6effd053f
Tyler Davis <tee.s.davis@gmail.com>
2017-01-31 14:41:16 -0800

    Make sails-neoj4 useable
    + fix tests and get create and createMany to work
    + add basic auth configuration to allow connection

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
+# Sails-Neo4j
+
+
+### Testing:
+WARNING: Please note that this will actually create and delete nodes in your local neo4j DB.
+However, They are tagged with the property { "sails_neo4j_test": 1 }, so if you tag any other nodes with that, (that is pretty weird but also) they will be deleted.
+
+```sh
+npm install
+cp .env.sample .env
+# IMPORTANT:
+# Make sure NEO4J_USERNAME and NEO4J_PASSWORD env vars (in .env file) actually
+# match your local neo4j DB credentials; these are needed for basic auth.
+npm test
+```
+
+### Copied from README.md of original repo in 2015:
+
 Sails-Neo4j Adapter; Will update the readme very soon.
 For now if you have any questions:
 [![Gitter chat](https://badges.gitter.im/natgeo/sails-neo4j.png)](https://gitter.im/natgeo/sails-neo4j)
--- test/create.js
+++ test/create.js
@@ -1,28 +1,49 @@
+require('dotenv').config();
+
 var assert = require('assert'),
 	adapter = require('../lib/adapter.js');
 
 describe('Creating Nodes', function () {
-	var nodes = [];
+	var nodeProps = { sails_neo4j_test: 1 };
 
  	before(function(done) {
-        var connection = {
-			identity: 'neo4j'
+    var connection = {
+			identity: 'neo4j',
+			username: process.env.NEO4J_USERNAME || 'neo4j',
+			password: process.env.NEO4J_PASSWORD || 'neo4j'
 		};
-		adapter.registerConnection(connection,null,done);
-    });
 
-	it('should create one node with a property test = "1"', function (done) {
-		adapter.create("neo4j", null, { test: 1 }, function(err, results) {
+		adapter.registerConnection(connection,null,done);
+  });
+
+	after(function() {
+		adapter.destroy("neo4j", null, nodeProps, function(err, results) {
 			if (err) { throw err; }
-			nodes.push(results);
+		});
+	});
+
+	it('should create one node with a property sails_neoj_test = 1', function (done) {
+		adapter.create("neo4j", null, nodeProps, function(err, results) {
+			if (err) { throw err; }
+
+			assert.equal(results.length, 1);
+
+			var props = results[0].n.data;
+			assert.deepEqual(props, nodeProps);
+
 			done();
 		});
 	});
 
-	it('should create multiple nodes with the property test = "1"', function(done) {
-		adapter.createMany("neo4j", null,{params:[{ test: 1 },{ test: 1 }]}, function(err, results) {
+	it('should create multiple nodes with the property sails_neoj_test = 1', function(done) {
+		adapter.createMany("neo4j", null, {props:[nodeProps,nodeProps]}, function(err, results) {
 			if (err) { throw err; }
-			nodes.push(results);
+
+			assert.equal(results.length, 2);
+
+			var props = results[0].n.data;
+			assert.deepEqual(props, nodeProps);
+
 			done();
 		});
 	});
