7f15ae0d7f78a5c5c2c06ce63677f3b267edeaa2
Tyler Davis <tee.s.davis@gmail.com>
2017-02-22 11:52:34 -0800

    update test, no func changes

--- test/unlink.js
+++ test/unlink.js
@@ -22,29 +22,46 @@ describe('Unlinking Nodes', function () {
       adapter.create(connectionName, null, nodeProps2, (err, results2) => {
         node2 = results2[0]
 
-        adapter.link(connectionName, null, nodeProps1, null, nodeProps2, relationshipType, {}, (err, results3) => {
-          relationship = results3[0]
-
-          done()
-        })
+        done()
       })
     })
   })
 
-	it('should unlink 2 nodes, and return the unlinked 2nd node(s)', function (done) {
-    adapter.unlink(connectionName, null, nodeProps1, null, nodeProps2, relationshipType, {}, (err, unlinkedNodes) => {
-      if (err) { done(err) }
-      assert.equal(unlinkedNodes.length, 1)
-      assert.equal(unlinkedNodes[0].id, node2.id)
-
-      adapter.getRelatedNodes(connectionName, null, nodeProps1, null, nodeProps2, relationshipType, {}, (err, relatedNodes) => {
-        if (err) { done(err) }
-
-        assert.equal(relatedNodes.length, 0)
+  describe('when the nodes are linked', () => {
+    beforeEach((done) => {
+      adapter.link(connectionName, null, nodeProps1, null, nodeProps2, relationshipType, {}, (err, results3) => {
+        relationship = results3[0]
 
         done()
       })
-
     })
-	})
+
+  	it('should unlink 2 nodes, and return the unlinked 2nd node(s)', function (done) {
+      adapter.unlink(connectionName, null, nodeProps1, null, nodeProps2, relationshipType, {}, (err, unlinkedNodes) => {
+        if (err) { done(err) }
+        assert.equal(unlinkedNodes.length, 1)
+        assert.equal(unlinkedNodes[0].id, node2.id)
+
+        adapter.getRelatedNodes(connectionName, null, nodeProps1, null, nodeProps2, relationshipType, {}, (err, relatedNodes) => {
+          if (err) { done(err) }
+
+          assert.equal(relatedNodes.length, 0)
+
+          done()
+        })
+      })
+  	})
+  })
+
+  describe('when the nodes are not linked', () => {
+  	it('should do nothing and return an empty array', function (done) {
+      adapter.unlink(connectionName, null, nodeProps1, null, nodeProps2, relationshipType, {}, (err, unlinkedNodes) => {
+        if (err) { done(err) }
+
+        assert.equal(unlinkedNodes.length, 0)
+
+        done()
+      })
+    })
+  })
 })
